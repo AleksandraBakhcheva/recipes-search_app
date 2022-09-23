8af11531ced181c0122b62430b21dd5fe99aa04c
Anna <momannuta@gmail.com>
2022-09-23 20:27:40 +0300

    render 1 random recipe

--- src/page1/js/index.js
+++ src/page1/js/index.js
@@ -7,7 +7,46 @@ let errorMessage = document.querySelector('.error_message');
 let arrow = document.querySelector('.hidden_arrow');
 let API_ID = "9469ba72";
 let API_KEY = "4313c2c37d93452e3b4523bb962f8253";
+const randomArr = [];
 
+document.addEventListener("DOMContentLoaded", function (){    
+    getRandom();
+});
+//запрос для вывода рандомных рецептов
+async function getRandom(){    
+    try{
+        //нужен цикл
+    let response = await fetch (`https://www.themealdb.com/api/json/v1/1/random.php`)
+    console.log(response)
+    let data = await response.json();
+    console.log(data.meals[0]);
+    //randomArr.push(data.meals[0]);
+    //generateHtml(data.meals);
+    let generatedHTML = '';
+    generatedHTML += `
+        <div class="result__item">
+            <img src="${data.meals[0].strMealThumb}" alt="${data.meals[0].strMeal}">
+            <div class="item__details">
+                <div class="details">
+                    <h2 class="item-name">${data.meals[0].strMeal}</h2>
+                    <h3 class="item-area">${data.meals[0].strArea}</h3>
+                </div>
+                <div class="view-button">
+                    <a href='page2/index.html?id=${data.meals[0].idMeal}'>View recipe</a>
+                </div>
+            </div>            
+        </div>`
+        result.innerHTML = generatedHTML;
+}catch (error) {
+        console.log(error.message);
+    }
+}
+// render рандомного рецепта
+//function renderRandom(results){
+    
+//==
+
+// поиск рецептов по клику на search
 searchButton.addEventListener('click', (e)=>{
     e.preventDefault();
     sendApiRequest();    
@@ -25,6 +64,7 @@ async function sendApiRequest(){
     searchForm.reset();
     console.log(data);    
 }
+//render по поиску
 function generateHtml(results){
     let generatedHTML = ''; 
     errorMessage.innerHTML = '';
@@ -40,7 +80,7 @@ function generateHtml(results){
                     <h3 class="item-area">${result.strArea}</h3>
                 </div>
                 <div class="view-button">
-                    <a href='../page2/index.html?id=${result.idMeal}'>View recipe</a>
+                    <a href='page2/index.html?id=${result.idMeal}'>View recipe</a>
                 </div>
             </div>            
         </div>`
@@ -52,3 +92,4 @@ function generateHtml(results){
 result.innerHTML = generatedHTML;
 }
 }
+
