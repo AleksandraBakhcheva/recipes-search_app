85bbd54a758868a31bc57a27ebe2e35ad564322b
Anna <momannuta@gmail.com>
2022-09-23 17:26:37 +0300

    localstorage

--- src/favorites_page/js/fav_page.js
+++ src/favorites_page/js/fav_page.js
@@ -1,3 +1,6 @@
+//import imgMarkerFilled from "../img/bookmark-filled.svg";
+//import imgMarkerNonFilled from "../img/bookmark.svg";
+// welcome==
 let email = localStorage.getItem("welcomeemail");
 let users = JSON.parse(localStorage.getItem('useremails') || '[]');
 let names = JSON.parse(localStorage.getItem('usernames') || '[]');
@@ -5,32 +8,34 @@ let i = users.indexOf(email);
 let div = document.createElement("div");
 document.querySelector("h1").append(div);
 div.textContent = `Welcome ${names[i]}`;
-
+//load from local storage==
 let result = document.querySelector('.result__container');
-let idArr = JSON.parse(localStorage.getItem('favorites') || '[]');
-console.log(idArr);
+let arrFavorites = JSON.parse(localStorage.getItem('favorites') || '[]');
+console.log(arrFavorites);
+console.log(typeof arrFavorites[1]);
 let dataArr = [];
 
-async function getRecipes(idArr) {    
-   try{
-    for (let i of idArr) {
+async function getRecipes() {    
+    try{
+    for (let i of arrFavorites) {
         let url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${i}`;
     const response = await fetch(url);
     const data = await response.json();    
         dataArr.push(data.meals[0]);
-   }}
-   catch(error) {
+    }}
+    catch(error) {
     console.error('Error:', error);
-  }
-   finally{
-    generateFav(dataArr);
-   }            
+    }
+    finally{
+    renderFav(dataArr);
+    }            
 }
 
-function generateFav(dataArr){
+function renderFav(dataArr){
     let generatedFav = ''; 
     result.innerHTML = '';
-    for (let d = 0; d < dataArr.length; d++){
+    console.log(dataArr);
+    for (let d = 0; d < dataArr.length; d++){        
             generatedFav += `
                 <div class="result__item">
                     <img src="${dataArr[d].strMealThumb}" alt="${dataArr[d].strMeal}">
@@ -39,7 +44,10 @@ function generateFav(dataArr){
                             <h2 class="item-name">${dataArr[d].strMeal}</h2>
                             <h3 class="item-area">${dataArr[d].strArea}</h3>
                         </div>
-                        <div class="bookmark"><img src="./img/bookmark-filled.svg" alt"fav"></div>
+                        <div class="recipe__marker">
+                            <button class="recipe__marker-btn" ><img src="./img/bookmark-filled.svg" alt="bookmark"
+                                    class="recipe__marker-btn-img" data-id=${dataArr[d].idMeal}></button>
+                        </div>
                         <div class="view-button">
                             <a href='../page2/index.html?id=${dataArr[d].idMeal}'>View recipe</a>
                         </div>
@@ -49,5 +57,43 @@ function generateFav(dataArr){
         result.innerHTML = generatedFav;
 }
 document.addEventListener("DOMContentLoaded", function (){  
-    getRecipes(idArr);
+    getRecipes(dataArr);
 });
+
+//bookmark=====
+
+    document.addEventListener('click', function(e){                
+        addFav(e);
+    });
+    document.onclick = function(e) {
+        if (e.target.classList.contains('recipe__marker-btn-img')){
+            e.target.classList.toggle('active');}
+        
+    }
+
+    function addFav(e) {
+        console.log(e.target.dataset.id);
+        if (e.target.classList.contains('active')){
+            arrFavorites.push(e.target.dataset.id);
+            e.target.src = "img/bookmark-filled.svg";
+        }else{
+            e.target.src = "img/bookmark.svg";
+            arrFavorites = arrFavorites.filter(el => el !== e.target.dataset.id);
+        }
+        localStorage.setItem('favorites', JSON.stringify(arrFavorites));
+       // checkFav(e);
+    }
+   /* function checkFav(e) {
+        if (arrFavorites) { // если в сторадже что-то есть, то парсим
+            arrFavorites = JSON.parse(arrFavorites);
+            if (arrFavorites.find(el => el == e.target.dataset.id)) {
+                console.log(arrFavorites.find(el => el == e.target.dataset.id));
+                this.className += ' active';
+                this.src = "img/bookmark-filled.svg";
+            }
+        } else {
+            // если нет, то присвоим дефолтное значение
+            arrFavorites = [];
+        }
+    
+    }*/
