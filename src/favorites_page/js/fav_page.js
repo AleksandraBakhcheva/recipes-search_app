e697471d05b6198c5cb0f64577f2010b0f3e3252
Anna <momannuta@gmail.com>
2022-09-23 12:04:28 +0300

    нарисовала избранное

--- src/favorites_page/js/fav_page.js
+++ src/favorites_page/js/fav_page.js
@@ -7,35 +7,47 @@ document.querySelector("h1").append(div);
 div.textContent = `Welcome ${names[i]}`;
 
 let result = document.querySelector('.result__container');
-let arrFavorites = JSON.parse(localStorage.getItem('favorites') || '[]');
-console.log(arrFavorites);
-//function load() {
-//for (let a of arrFavorites) {
-    //console.log(a);    
-document.addEventListener("DOMContentLoaded", function (){    
-        
-fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${arrFavorites[0]}`)
-    .then((response) =>{
-        return response.json();}).then((data) =>{
-            console.log(data.meals);
-            let generatedFav = ''; 
-            result.innerHTML = ''; 
-            for (let d of data.meals){
-                generatedFav += `
+let idArr = JSON.parse(localStorage.getItem('favorites') || '[]');
+console.log(idArr);
+let dataArr = [];
+
+async function getRecipes(idArr) {    
+   try{
+    for (let i of idArr) {
+        let url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${i}`;
+    const response = await fetch(url);
+    const data = await response.json();    
+        dataArr.push(data.meals[0]);
+   }}
+   catch(error) {
+    console.error('Error:', error);
+  }
+   finally{
+    generateFav(dataArr);
+   }            
+}
+
+function generateFav(dataArr){
+    let generatedFav = ''; 
+    result.innerHTML = '';
+    for (let d = 0; d < dataArr.length; d++){
+            generatedFav += `
                 <div class="result__item">
-                    <img src="${d.strMealThumb}" alt="${d.strMeal}">
+                    <img src="${dataArr[d].strMealThumb}" alt="${dataArr[d].strMeal}">
                     <div class="item__details">
                         <div class="details">
-                            <h2 class="item-name">${d.strMeal}</h2>
-                            <h3 class="item-area">${d.strArea}</h3>
+                            <h2 class="item-name">${dataArr[d].strMeal}</h2>
+                            <h3 class="item-area">${dataArr[d].strArea}</h3>
                         </div>
                         <div class="bookmark"><img src="./img/bookmark-filled.svg" alt"fav"></div>
                         <div class="view-button">
-                            <a href='../page2/index.html?id=${d.idMeal}'>View recipe</a>
+                            <a href='../page2/index.html?id=${dataArr[d].idMeal}'>View recipe</a>
                         </div>
                     </div>            
                 </div>`
-            }           
+            }
         result.innerHTML = generatedFav;
-        }) 
+}
+document.addEventListener("DOMContentLoaded", function (){  
+    getRecipes(idArr);
 });
