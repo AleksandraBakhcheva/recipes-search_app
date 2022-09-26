c2391ef402bdafc52957a7937911a3e69d5d8ba1
Inga <piavka91@gmail.com>
2022-09-26 02:23:23 +0200

    add styles for print, correct saving to favourites

--- src/recipe_page/js/index.js
+++ src/recipe_page/js/index.js
@@ -1,9 +1,9 @@
 import imgMarkerFilled from "../img/heart_red.png";
 import imgMarkerNonFilled from "../img/heart.png";
 
-//const id = 52959; //для проверки
+const id = 52959; //для проверки
 //const id = 52771;
-const id = 53043;
+//const id = 53043;
 
 // let params = (new URL(document.location)).searchParams;
 // const id = Number(params.get("id"));
@@ -13,10 +13,11 @@ let foodName = document.querySelector(".recipe__name");
 let foodArea = document.querySelector(".recipe__area");
 let foodCategory = document.querySelector(".recipe__category");
 let foodTags = document.querySelector(".recipe__tags");
-
 let ingredients = document.querySelector(".recipe__ingredients");
-
 const bookmark = document.querySelector(".recipe__marker-btn-img");
+
+let email = localStorage.getItem("welcomeemail");
+//let email = "sasha@mail.ru"; //для проверки
 let arrFavorites = localStorage.getItem('favorites');
 
 async function loadData() {
@@ -30,64 +31,86 @@ async function loadData() {
 
     document.title = data.meals[0].strMeal + " Recipe";
 
-    let screen = window.matchMedia("(max-width: 992px)")
-    myFunction(screen, data); // Call listener function at run time
-    screen.addListener(myFunction); // Attach listener function on state changes
-
     render(data);
-    checkFavorites(data);
 
+    // let massiv = [{
+    //         email: 'Inga_petrova91@mail.ru',
+    //         favRecipes: [2959, 52771, 53043]
+    //     },
+    //     {
+    //         email: 'chuka@mail.ru',
+    //         favRecipes: [2959, 53043]
+    //     },
+    //     {
+    //         email: 'sasha@mail.ru',
+    //         favRecipes: []
+    //     },
+    // ]
+    // localStorage.setItem('favorites', JSON.stringify(massiv));
 
     const bookmarkBtn = document.querySelector(".recipe__marker-btn");
-    bookmarkBtn.addEventListener("click", function () {
-        addToFavorites(data);
-    });
+    const message = document.querySelector(".recipe__message");
+    if (email) {
+
+        arrFavorites = checkFavorites();
+        bookmarkBtn.addEventListener("click", function () {
+            addToFavorites(data);
+        });
+    } else {
+        bookmarkBtn.addEventListener('mouseover', (event) => {
+            message.style.display = "block";
+        });
+        bookmarkBtn.addEventListener('mouseout', (event) => {
+            message.style.display = "none";
+        });
+    }
 }
+
 
 function addToFavorites(data) { //при нажатии на кнопку
-    if (!bookmark.classList.contains('active')) { //если нет класса active
-        bookmark.className += ' active'; //то добавляем ему этот класс
-        bookmark.src = imgMarkerFilled; // картинка пустая
-        arrFavorites.push(id); //добавляем в массив 
-    } else { //если нет
-        bookmark.className = 'recipe__marker-btn'; //то класс без active
-        bookmark.src = imgMarkerNonFilled; // картинка заполненная
-        arrFavorites = arrFavorites.filter(el => el !== id); //удаляем из массива
+    let index = arrFavorites.findIndex((el) => el.email === email);
+    if (index > -1) {
+        {
+            if (!bookmark.classList.contains('active')) { //если нет класса active
+                bookmark.className += ' active'; //то добавляем ему этот класс
+                bookmark.src = imgMarkerFilled; // картинка пустая
+                arrFavorites[index].favRecipes.push(id);
+            } else {
+                bookmark.className = 'recipe__marker-btn'; //то класс без active
+                bookmark.src = imgMarkerNonFilled; // картинка заполненная
+                arrFavorites[index].favRecipes = arrFavorites[index].favRecipes.filter((el) => el !== id); //удаляем из массива
+            }
+        }
+        localStorage.setItem('favorites', JSON.stringify(arrFavorites));
     }
-    localStorage.setItem('favorites', JSON.stringify(arrFavorites));
-
 }
 
-function checkFavorites(data) { //вызывается при загрузке страницы
+function checkFavorites() {
 
-    if (arrFavorites) { // если в сторадже что-то есть, то парсим
+    if (arrFavorites) { // если в сторадже что-то есть
         arrFavorites = JSON.parse(arrFavorites);
-        if (arrFavorites.find(el => el == id)) { //если элемент есть в локал сторадж
-            //console.log(arrFavorites.find(el => el == id));
+    } else {
+        arrFavorites = [{
+            email: email,
+            favRecipes: []
+        }]
+    }
+
+    let index = arrFavorites.findIndex((el) => el.email === email);
+    if (index > -1) {
+        let arrive = arrFavorites[index].favRecipes.slice();
+        if (arrive.find((el) => el == id)) { //если элемент есть в локал сторадж
             bookmark.className += ' active';
             bookmark.src = imgMarkerFilled;
         }
     } else {
-        // если нет, то присвоим дефолтное значение
-        arrFavorites = [];
+        arrFavorites.push({
+            email: email,
+            favRecipes: []
+        });
+        console.log(arrFavorites)
     }
-
-}
-
-function myFunction(screen, data) {
-    let Bigscreen = document.querySelector(".bigscreen");
-    //console.log(Bigscreen)
-    let Smallscreen = document.querySelector(".smallscreen");
-    //console.log(Smallscreen)
-    if (screen.matches) { // If media query matches
-        //console.log(screen)
-        //Smallscreen.className += ' recipe__instructions';
-        //recipe__instructions.className += 'bigscreen';
-    } else {
-        //console.log(screen)
-        // Bigscreen.className += ' recipe__instructions';
-        //recipe__instructions.className += 'smallscreen';
-    }
+    return arrFavorites;
 }
 
 function render(data) {
@@ -101,14 +124,13 @@ function render(data) {
     foodYouTube.href = data.meals[0].strYoutube;
 
     //rendering instruction
+
     let instructions = document.querySelectorAll(".recipe__instructions");
     for (let index = 0; index < instructions.length; index++) {
         const instructionSpace = document.createElement('p');
         instructionSpace.innerHTML = data.meals[0].strInstructions;
         instructions[index].append(instructionSpace);
     }
-
-
 
     //rendering ingredients
 
@@ -133,19 +155,10 @@ function render(data) {
     }
 }
 
-
-
-loadData();
-
-const login = "inga";
-const message = document.querySelector(".recipe__message");
-
-addEventListener('mouseout', (event) => {});
-
-// try {
-//     loadData();
-// } catch (e) {
-//     console.log("error");
-// } finally {
-//     console.log('We do cleanup here');
-// }
+try {
+    loadData();
+} catch (e) {
+    console.log("error");
+} finally {
+    console.log('We do cleanup here');
+}
