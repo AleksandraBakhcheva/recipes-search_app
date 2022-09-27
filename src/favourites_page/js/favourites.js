117201520593370b8942324d5deff3425de4e006
Inga <piavka91@gmail.com>
2022-09-27 03:35:55 +0200

    correct favourites

--- src/favourites_page/js/favourites.js
+++ src/favourites_page/js/favourites.js
@@ -5,85 +5,104 @@ import imgMarkerNonFilled from "../../recipe_page/img/heart.png";
 let email = localStorage.getItem("welcomeemail");
 let users = JSON.parse(localStorage.getItem('useremails') || '[]');
 let names = JSON.parse(localStorage.getItem('usernames') || '[]');
+let i = users.indexOf(email);
 
 let welcomeMsg = document.querySelector("h1");
 let result = document.querySelector('.result__container');
+let res = document.querySelector('.result');
 
 let arrFavorites = localStorage.getItem('favorites');
 
 if (email) {
-    welcomeMsg.textContent = `Welcome, ${names[0]}`;
+    welcomeMsg.textContent = `Welcome, ${names[i]}`;
     welcomeMsg.classList.add('welcome-msg');
+    if (arrFavorites) { // если в сторадже что-то есть
+        arrFavorites = JSON.parse(arrFavorites);
+    } else {
+        document.querySelector("h2").textContent = "No favourites";
+        arrFavorites = [{
+            email: email,
+            favRecipes: []
+        }]
+    }
 } else {
-    document.querySelector("h2").textContent = "Необходимо зарегистрироваться";
+    document.querySelector("h2").textContent = "You need to register. No email";
 };
 
 
-if (arrFavorites) { // если в сторадже что-то есть
-    arrFavorites = JSON.parse(arrFavorites);
-} else {
-    arrFavorites = [{
-        email: email,
-        favRecipes: []
-    }]
-}
-console.log(arrFavorites);
 
 let array = [];
 
 let index = arrFavorites.findIndex((el) => el.email === email);
 if (index > -1) {
     array = arrFavorites[index].favRecipes.slice();
+}
+// } else {
+//     document.querySelector("h2").textContent = "You need to register";
+// }
 
-} else {
-    document.querySelector("h2").textContent = "Необходимо зарегистрироваться";
+let div = document.createElement("div");
+for (let arr of array) {
+    div.textContent += `${arr}, `;
 }
 
-// async function getRecipes() {
-//     try {
-//         for (let index of arrFavorites) {
-//             let url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${index}`;
-//             const response = await fetch(url);
-//             const data = await response.json();
-//             dataArr.push(data.meals[0]);
-//         }
-//     } catch (error) {
-//         console.error('Error:', error);
-//     } finally {
-//         renderFav(dataArr);
-//     }
-// }
+res.append(div);
 
-// function renderFav(dataArr) {
-//     let generatedFav = '';
-//     result.innerHTML = '';
-//     console.log(dataArr);
-//     for (let d = 0; d < dataArr.length; d++) {
-//         generatedFav += `
-//                 <div class="result__item">
-//                     <img src="${dataArr[d].strMealThumb}" alt="${dataArr[d].strMeal}">
-//                     <div class="item__details">
-//                         <div class="details">
-//                             <h2 class="item-name">${dataArr[d].strMeal}</h2>
-//                             <h3 class="item-area">${dataArr[d].strArea}</h3>
-//                         </div>
-//                         <div class="recipe__marker">
-//                             <button class="recipe__marker-btn" ><img src="./img/bookmark-filled.svg" alt="bookmark"
-//                                     class="recipe__marker-btn-img" data-id=${dataArr[d].idMeal}></button>
-//                         </div>
-//                         <div class="view-button">
-//                             <a href='../recipe_page/index.html?id=${dataArr[d].idMeal}'>View recipe</a>
-//                         </div>
-//                     </div>            
-//                 </div>`
-//     }
-//     result.innerHTML = generatedFav;
-// }
-// document.addEventListener("DOMContentLoaded", function () {
-//     getRecipes(dataArr);
-// });
+let dataArr = [];
 
-// //bookmark=====
+async function getRecipes() {
+    try {
+        for (let id of array) {
+            let url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+            const response = await fetch(url);
+            const data = await response.json();
+            dataArr.push(data.meals[0]);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    } finally {
+        renderFav(dataArr);
+    }
+}
+
+function renderFav(dataArr) {
+    let generatedFav = '';
+    result.innerHTML = '';
+    console.log(dataArr);
+    for (let d = 0; d < dataArr.length; d++) {
+        generatedFav += `
+                <div class="result__item">
+                    <img src="${dataArr[d].strMealThumb}" alt="${dataArr[d].strMeal}">
+                    <div class="item__details">
+                        <div class="details">
+                            <h2 class="item-name">${dataArr[d].strMeal}</h2>
+                            <h3 class="item-area">${dataArr[d].strArea}</h3>
+                        </div>
+                        
+                        <div class="view-button">
+                            <a href='../recipe_page/recipe.html?id=${dataArr[d].idMeal}'>View recipe</a>
+                        </div>
+                    </div>            
+                </div>`
+    }
+    result.innerHTML = generatedFav;
+}
+
+/* <div class = "recipe__marker" >
+    <
+    button class = "recipe__marker-btn" > < img src = "./img/bookmark-filled.svg"
+alt = "bookmark"
+class = "recipe__marker-btn-img"
+data - id = $ {
+        dataArr[d].idMeal
+    } > < /button> <
+    /div> */
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    getRecipes(dataArr);
+});
+
 
 // document.addEventListener('click', function (e) {
 //     if (e.target.classList.contains('recipe__marker-btn-img')) {
